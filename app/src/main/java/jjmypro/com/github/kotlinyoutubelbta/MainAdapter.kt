27d3b7e1d600868e1a7cc6f574a38ff44ea3172f
Jesper Hjort <jj.mypro@gmail.com>
2018-11-13 14:29:06 +0100

    Intro to RecyclerView (Ep1)

---
+++ app/src/main/java/jjmypro/com/github/kotlinyoutubelbta/MainAdapter.kt
@@ -0,0 +1,33 @@
+package jjmypro.com.github.kotlinyoutubelbta
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.video_row.view.*
+
+class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
+
+    val videoTitles = listOf("First title", "Second", "3rd", "More titles...")
+
+    // numberOfItems
+    override fun getItemCount(): Int {
+        return videoTitles.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        // how do we even create a view
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
+        return CustomViewHolder(cellForRow)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        val videoTitle = videoTitles.get(position)
+        holder.view.textView_video_title.text = videoTitle
+    }
+}
+
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+}
